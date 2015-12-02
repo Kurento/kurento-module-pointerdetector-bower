f0f9b46a174455fbc7b16c6690fbea7c8dea407d
jenkins <jenkins@kurento.org>
2015-12-02 13:37:50 +0000

    Generated code from kurento-module-pointerdetector-js:71078c5

--- js/kurento-module-pointerdetector.js
+++ js/kurento-module-pointerdetector.js
@@ -566,7 +566,7 @@ exports.WindowParam = WindowParam;
  */
 
 Object.defineProperty(exports, 'name',    {value: 'pointerdetector'});
-Object.defineProperty(exports, 'version', {value: '6.2.0'});
+Object.defineProperty(exports, 'version', {value: '6.2.1-dev'});
 
 
 var PointerDetectorFilter = require('./PointerDetectorFilter');
